95c50ae9dc38f366a454f38a4fab6e21a0625140
Thushani Jayasekera <thushanij.18@cse.mrt.ac.lk>
2024-09-19 15:02:24 +0530

    add extra go service

---
+++ go_async/example/websockets.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	// CheckOrigin can be added if you need to handle CORS.
+	CheckOrigin: func(r *http.Request) bool {
+		return true // Adjust this according to your CORS policy
+	},
+}
+
+func main() {
+	http.HandleFunc("/echo", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("WebSocket endpoint hit")
+		// Upgrade HTTP server connection to the WebSocket protocol
+		conn, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			log.Printf("error upgrading connection: %v", err)
+			return
+		}
+		defer conn.Close() // Ensure the connection is closed when the function returns
+
+		for {
+			// Read message from browser
+			msgType, msg, err := conn.ReadMessage()
+			if err != nil {
+				log.Printf("error reading message: %v", err)
+				break // Exit the loop if there's an error
+			}
+
+			// Print the message to the console
+			fmt.Printf("%s sent: %s\n", conn.RemoteAddr(), string(msg))
+
+			// Write message back to browser
+			if err = conn.WriteMessage(msgType, msg); err != nil {
+				log.Printf("error writing message: %v", err)
+				break // Exit the loop if there's an error
+			}
+		}
+	})
+
+	http.HandleFunc("/echo/new", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("WebSocket new endpoint hit")
+		// Upgrade HTTP server connection to the WebSocket protocol
+		conn, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			log.Printf("error upgrading connection: %v", err)
+			return
+		}
+		defer conn.Close() // Ensure the connection is closed when the function returns
+
+		for {
+			// Read message from browser
+			msgType, msg, err := conn.ReadMessage()
+			if err != nil {
+				log.Printf("error reading message: %v", err)
+				break // Exit the loop if there's an error
+			}
+
+			// Print the message to the console
+			fmt.Printf("%s sent: %s\n", conn.RemoteAddr(), string(msg))
+
+			// Write message back to browser
+			if err = conn.WriteMessage(msgType, msg); err != nil {
+				log.Printf("error writing message: %v", err)
+				break // Exit the loop if there's an error
+			}
+		}
+	})
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "websockets.html")
+	})
+
+	// Start the HTTP server on port 8009
+	log.Println("Server started on port 6789")
+	err := http.ListenAndServe(":6789", nil)
+	if err != nil {
+		log.Fatalf("error starting server: %v", err)
+	}
+}
