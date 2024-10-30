e4e9dd659681814eb99a49a50484eae2a29b57f6
Thushani Jayasekera <thushanij.18@cse.mrt.ac.lk>
2024-10-30 18:03:21 +0530

    get token

--- go_client_service/main.go
+++ go_client_service/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
@@ -8,30 +9,37 @@ import (
 	"time"
 
 	"github.com/gorilla/websocket"
+	"golang.org/x/oauth2/clientcredentials"
 )
 
 func main() {
 	// Read environment variables
 	serviceURL := os.Getenv("SVC_URL")
-	// consumerKey := os.Getenv("CONSUMER_KEY")
-	// consumerSecret := os.Getenv("CONSUMER_SECRET")
-	// tokenUrl := os.Getenv("TOKEN_URL")
+	consumerKey := os.Getenv("CONSUMER_KEY")
+	consumerSecret := os.Getenv("CONSUMER_SECRET")
+	tokenUrl := os.Getenv("TOKEN_URL")
 	choreoApiKey := os.Getenv("CHOREO_API_KEY")
 
 	// Configure OAuth2 client credentials
-	// clientCredsConfig := clientcredentials.Config{
-	// 	ClientID:     consumerKey,
-	// 	ClientSecret: consumerSecret,
-	// 	TokenURL:     tokenUrl,
-	// }
-	// client := clientCredsConfig.Client(context.Background())
+	clientCredsConfig := clientcredentials.Config{
+		ClientID:     consumerKey,
+		ClientSecret: consumerSecret,
+		TokenURL:     tokenUrl,
+	}
+	// Retrieve the access token
+	ctx := context.Background()
+	token, err := clientCredsConfig.Token(ctx)
+	if err != nil {
+		log.Fatal("Error getting token:", err)
+		return
+	}
+	fmt.Println("Connecting to service:", serviceURL, token)
 
-	fmt.Println("Connecting to service:", serviceURL, choreoApiKey)
-
+	bearerToken := `Bearer` + token.AccessToken
 	// Configure headers, adding the API key
 	headers := http.Header{
 		"Choreo-API-Key": []string{choreoApiKey},
-		// "Test-Key":       []string{"eyJraWQiOiJnYXRld2F5X2NlcnRpZmljYXRlX2FsaWFzIiwiYWxnIjoiUlMyNTYifQ.eyJzdWIiOiJhMzllYTQxYS00MzgyLTQ3ZWQtODRiZi01NjM3NjE2ODk1NWRAY2FyYm9uLnN1cGVyIiwiYXVkIjoiY2hvcmVvOmRlcGxveW1lbnQ6c2FuZGJveCIsImlzcyI6Imh0dHBzOlwvXC9zdHMucHJldmlldy1kdi5jaG9yZW8uZGV2OjQ0M1wvYXBpXC9hbVwvcHVibGlzaGVyXC92MlwvYXBpc1wvaW50ZXJuYWwta2V5Iiwia2V5dHlwZSI6IlNBTkRCT1giLCJzdWJzY3JpYmVkQVBJcyI6W3sic3Vic2NyaWJlclRlbmFudERvbWFpbiI6bnVsbCwibmFtZSI6ImNvbXBvbmVudHlhbWwgLSBnb3dlYnNvY2tldGFwaSIsImNvbnRleHQiOiJcL2YzNGZjMDExLTM4YTYtNDY2My05ZmExLTlhMjQzMDA2YzUzOFwvd2Vic29ja2V0cHJvamVjdFwvY29tcG9uZW50eWFtbFwvdjEuMCIsInB1Ymxpc2hlciI6ImNob3Jlb19kZXZfYXBpbV9hZG1pbiIsInZlcnNpb24iOiJ2MS4wIiwic3Vic2NyaXB0aW9uVGllciI6bnVsbH1dLCJleHAiOjE3MzAyODg0MzQsInRva2VuX3R5cGUiOiJJbnRlcm5hbEtleSIsImlhdCI6MTczMDI4NzgzNCwianRpIjoiZWY3MDAzNzQtNGNlYi00Y2ExLWEyMjQtNWY2ZDRjODI2NjM3In0.hKjECY8zmxGkl5vVidg1uCfNgT08DJxrRcQl78zQ8tidgGXdrmPTNNhXkvP8t2DI_2op0jn7mEL_yZd9BzjkbOutHlkvNovhLZtAsmAc0LkJt529XTsxJhbnlz2wcrMFw1gMS2iRt2nSjUYAYVTZYBU4zq537G1vlOXCydagNjCzFdYuTMoQJbjdA--WSO6WzpgIg2GkJ1k14UXdjPxuxNTr014m5o5agkiQPGvDHoZgjk_r3lgCxr9ruDd0G5mm4hG0f7HoxAsrRTQNHuSttsrtsb-RpF_4B7ucmuAv5XZePsigkJXfxbHAJybK5pZQx1DZ4LvYLOwo8Klan_QAYMyIv-5WCTvc-vMsprrXQCFgr1MVygbxTKOk0bbt4tVm8JPhwyWwvHlU6xUqMeap5aWLq5OPYVg0E_vNRyezftyY_TA2HxuZ4CrUjaXG4W-bUJImf9NvNyDvrzA0v1Ph2ZUeYdoKi9VNPQMNQt2wbIQKZgVVdkaSKxUBuiDpuaXkc7rDBziZSZ97iJ9Nj8jrRpRrObG3MfCcAFhG4uV36mWXDGm5mBZhtKiEPPscye3GtCoCKF6RYf5BuR3PnrVe9EY_r7YOve8lcj0RWtiTmMnfbR0PZJKbz6rh2WJYSgox2BnXW2D2YARk6xilyt62yvoTGjFrpVD1kQFuQPYQdyo"},
+		"Authorization":  []string{bearerToken},
 	}
 
 	// Establish the WebSocket connection using DefaultDialer
