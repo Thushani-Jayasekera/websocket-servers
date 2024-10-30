9949b2cf7a2d9c184b9ff7d9324f7f8b4fb6703a
Thushani Jayasekera <thushanij.18@cse.mrt.ac.lk>
2024-10-30 17:19:08 +0530

    Add retrying

--- go_client_service/main.go
+++ go_client_service/main.go
@@ -36,35 +36,47 @@ func main() {
 
 	// Establish the WebSocket connection using DefaultDialer
 	connectionURL := serviceURL + "/echo"
-	conn, _, err := websocket.DefaultDialer.Dial(connectionURL, headers)
-	if err != nil {
-		log.Fatal("Dial error:", err)
-		return
-	}
-	defer conn.Close()
-
-	// Function to send messages periodically
-	go func() {
-		ticker := time.NewTicker(2 * time.Second) // Sends a message every 2 seconds
-		defer ticker.Stop()
-		for range ticker.C {
-			message := `{"currency":"EURUSD"}`
-			err := conn.WriteMessage(websocket.TextMessage, []byte(message))
-			if err != nil {
-				log.Println("Write error:", err)
-				return
-			}
-			fmt.Println("Sent:", message)
-		}
-	}()
-
-	// Listen for incoming messages
 	for {
-		_, message, err := conn.ReadMessage()
+		// Attempt to connect
+		conn, _, err := websocket.DefaultDialer.Dial(connectionURL, headers)
 		if err != nil {
-			log.Println("Read error:", err)
-			return
+			log.Println("Dial error, retrying in 5 seconds:", err)
+			time.Sleep(5 * time.Second) // Wait before retrying
+			continue
 		}
-		fmt.Printf("Received: %s\n", message)
+		log.Println("Connected to WebSocket server")
+
+		// Ensure the connection is closed when done
+		defer conn.Close()
+
+		// Start the message sending goroutine
+		go func(conn *websocket.Conn) {
+			ticker := time.NewTicker(2 * time.Second) // Sends a message every 2 seconds
+			defer ticker.Stop()
+			for range ticker.C {
+				message := `{"currency":"EURUSD"}`
+				err := conn.WriteMessage(websocket.TextMessage, []byte(message))
+				if err != nil {
+					log.Println("Write error:", err)
+					return
+				}
+				fmt.Println("Sent:", message)
+			}
+		}(conn)
+
+		// Listen for incoming messages
+		for {
+			_, message, err := conn.ReadMessage()
+			if err != nil {
+				log.Println("Read error, reconnecting:", err)
+				break // Exit the loop to reconnect
+			}
+			fmt.Printf("Received: %s\n", message)
+		}
+
+		// Wait before retrying the connection
+		time.Sleep(5 * time.Second)
+		log.Println("Reconnecting to WebSocket server...")
 	}
+
 }
